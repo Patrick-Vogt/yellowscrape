114fca5fec44b0e858062e3f34520658ab28231d
Diego Carrasco G <557703+dacog@users.noreply.github.com>
2019-03-03 12:16:13 +0100

    Update ylp.py
    when runned, TabError: inconsistent use of tabs and spaces in indentation
    fixed using sublime text to change all identation to spaces.

--- gelbeseiten/spiders/ylp.py
+++ gelbeseiten/spiders/ylp.py
@@ -12,16 +12,13 @@ class YlpSpider(scrapy.Spider):
         for x in range (1,3000)]
     
     def parse(self, response):
-		#companies = response.xpath('//*[@class="name m08_name"]')
-		
-		for company in response.css('article'):
-			name = company.xpath('.//span[@itemprop="name"]//text()').extract_first()
-			address = company.xpath('.//span[@itemprop="streetAddress"]//text()').extract_first()
-			postalcode = company.xpath('.//span[@itemprop="postalCode"]//text()').extract_first()
-			addressLocality = company.xpath('.//span[@itemprop="addressLocality"]//text()').extract_first()
-			phone = company.xpath('.//span[@class="nummer"]//text()').extract_first()
-			mail = company.xpath('.//a[@class="link email_native_app"]/@href').extract()
-			branchen = company.xpath('.//div[@data-role="branchen"]/div/span/text()').extract_first()
-
-
-			yield{'Name':name,'Address':address,'PLZ':postalcode,'Ort':addressLocality,'Tel':phone, 'Mail':mail, 'Branche': branchen }
+        #companies = response.xpath('//*[@class="name m08_name"]')
+        for company in response.css('article'):
+            name = company.xpath('.//span[@itemprop="name"]//text()').extract_first()
+            address = company.xpath('.//span[@itemprop="streetAddress"]//text()').extract_first()
+            postalcode = company.xpath('.//span[@itemprop="postalCode"]//text()').extract_first()
+            addressLocality = company.xpath('.//span[@itemprop="addressLocality"]//text()').extract_first()
+            phone = company.xpath('.//span[@class="nummer"]//text()').extract_first()
+            mail = company.xpath('.//a[@class="link email_native_app"]/@href').extract()
+            branchen = company.xpath('.//div[@data-role="branchen"]/div/span/text()').extract_first()
+            yield{'Name':name,'Address':address,'PLZ':postalcode,'Ort':addressLocality,'Tel':phone, 'Mail':mail, 'Branche': branchen }
